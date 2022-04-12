7b09d36769ee820205d954a674549b8e6d58edee
JessieJessJe <jiahuihan@yahoo.com>
2022-04-12 01:31:19 -0400

    addBox

--- src/components/VizThree.js
+++ src/components/VizThree.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useContext, useRef, useLayoutEffect } from 'react';
 
 import { Canvas, useFrame, extend, useThree } from '@react-three/fiber'
-import { TrackballControls, Line, Segments } from "@react-three/drei";
+import { TransformControls, OrbitControls, TrackballControls, OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from 'three'
-import { normLat, normLong } from "../utils/helper"
+import { normLat, normLong, normZ, groupColor } from "../utils/helper"
 import { SectionContext } from '../utils/sectionContext';
 import useScrollPosition from "../utils/useScrollPosition";
 
 import NYCGeoJson from "../data/nyc.json"
+import data from "../data/mydata.json"
 
 let key = 0;
 
@@ -18,7 +19,7 @@ function Box(props) {
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
     // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((state, delta) => (mesh.current.rotation.x += 0.001))
+    // useFrame((state, delta) => (mesh.current.rotation.x += 0.001))
     // Return view, these are regular three.js elements expressed in JSX
     return (
       <mesh
@@ -33,6 +34,27 @@ function Box(props) {
       </mesh>
     )
   }
+
+
+function Nodes(){
+    let nodes = [];
+        for (let key in data){
+                
+            let pX = - normLong(data[key].long),
+                pY = normLat(data[key].lat),
+                pZ = normZ(data[key].year),
+                pColor = groupColor[data[key].group];
+            
+            nodes.push(<Box key={`${pX}+${pY}+${pZ}`} position={[pX, pY, pZ]} />)
+            
+            }
+        
+        return(
+            <React.Fragment>
+            {nodes}
+            </React.Fragment>
+        )
+}
 
 function BoroughMap({shape}){
     let linelist = [];
@@ -49,8 +71,6 @@ function BoroughMap({shape}){
         })
         console.log(linelist)
         ref.current.setFromPoints(linelist)
-
-        // geometry = new THREE.BufferGeometry().setFromPoints(linelist)
         
     }, [])
 
@@ -58,7 +78,7 @@ function BoroughMap({shape}){
 
     return (
 
-    // <line key={key} geometry={geometry}>
+ 
     <line key={key} rotation-y={Math.PI}>
     <bufferGeometry ref={ref}/>
     <lineBasicMaterial color="white"  lineWidth={1} />
@@ -73,7 +93,7 @@ function NYCMap(){
         NYCGeoJson.features.forEach((feature)=>{
             feature.geometry.coordinates.forEach((shape)=>{
                 // wireframe(shape, mapM)
-                boroughs.push(< BoroughMap shape={shape} key={key}/>)
+                boroughs.push(< BoroughMap shape={shape} key={ `${key + shape[0][0][1]}+nyc`}/>)
          
             })
         })
@@ -91,39 +111,51 @@ function NYCMap(){
 
 function VizThree(){
 
-    const [nycdata, setNYCdata ] = useState([NYCGeoJson]);
+    const [camera, setCamera ] = useState(true);
 
     const { setSection } = useContext(SectionContext);
     const scrollPosition = useScrollPosition();
     const margin_left = 0.2 * window.innerWidth;
 
     const aspect = window.innerWidth / window.innerHeight;
-    let nycmap;
-    nycmap = NYCMap();
 
+    const toggleCamera = ()=>{
+        setCamera(!camera)
+    }
+
+    const OrthographicCamera_ratio = 8
 
     return(
         <React.Fragment>
 
         <div id='three-wrapper'>
 
+        <button id='three-cam-btn' onClick={toggleCamera}>Switch View</button>
+
         <Canvas
       
-          camera = { {fov:80, near: 1, far: 1000, position: [0, 0, 120]} }      
+        //   camera = { {fov:80, near: 1, far: 1000, position: [0, 0, 120]} }      
         >
+        <PerspectiveCamera 
+            makeDefault = {camera}
+            fov={80} near={1} far={1000} position={[0, 0, 120]} />
 
+        <OrthographicCamera
+            makeDefault = {!camera}
+            left={-window.innerWidth / OrthographicCamera_ratio}
+            right={window.innerWidth / OrthographicCamera_ratio}
+            top={window.innerHeight / OrthographicCamera_ratio}
+            bottom={-window.innerHeight / OrthographicCamera_ratio}
+            near={1} far={1000} position={[0, -80, 0]} />
 
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
  
-        <TrackballControls autoRotate autoRotateSpeed={5} />s
-       
-        <Box position={[-0.2, 0, 0]} />
-        <Box position={[25.213108706880842, -73.93061327113944, 1]} />  
+        <OrbitControls />
+
+        <Nodes />
 
         <NYCMap />
-
-        {/* <BoroughMap shape={ NYCGeoJson.features[0].geometry.coordinates[0]} nycdata={NYCGeoJson}/> */}
 
         </Canvas>
 
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -17,3 +17,23 @@ export function normLong(long){
     var norm = ((long-min) / (max - min) * 50 -25)*3.5
     return norm
     }
+
+export  const groupColor ={
+        "-1":0xcccccc,
+        "0":0x3440eb,
+        "1":0x9ca2ff,
+        "2":0xfc53bc,
+        "3":0xb20061,
+        "4":0x3440eb,
+        "5":0xffcf0a, 
+        "6":0x3440eb,
+        "7":0x34ebeb,
+        "8":0xe67a00};
+
+
+
+export function normZ(year){
+
+    var norm = (year - 2000 + 3)* 1.5
+    return norm
+    }
