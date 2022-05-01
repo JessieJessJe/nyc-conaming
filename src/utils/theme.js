02e72205898e8322f96577c5bc86c9d0238e3fae
JessieJessJe <jiahuihan@yahoo.com>
2022-05-01 10:44:49 -0400

    data

---
+++ src/utils/theme.js
@@ -0,0 +1,34 @@
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const ThemeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#000000',
+      light: '#000000',
+      dark: '#000000',
+    },
+    secondary: {
+      main: '#000000',
+      light: '#000000',
+      dark: '#000000',
+    },
+    text: {
+      primary: '#000000',
+      secondary: '#000000',
+      disabled: '#000000',
+      hint: '#000000',
+    },
+  },
+  typography: {
+    fontFamily: 'EB Garamond',
+  },
+  shape: {
+    borderRadius: 0,
+  },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true,
+    },
+  },
+};
