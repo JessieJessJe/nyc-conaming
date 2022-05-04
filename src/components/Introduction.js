06ea3e305bc01c408b170d53faadf1bfabf144fe
JessieJessJe <jiahuihan@yahoo.com>
2022-05-04 17:14:43 -0400

    transition

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,6 +4,7 @@ import { motion, LayoutGroup, AnimatePresence} from 'framer-motion';
 
 import './imgPlates.css'
 import './textPlates.css'
+import './intro.css'
 
 import useScrollPosition from "../utils/useScrollPosition";
 
@@ -38,14 +39,15 @@ const plateInfo=[
     "reason":`This co-naming honors the thousands of dedicated medical and nursing professionals, EMT\u2019s, social workers, administrators, custodial and food service staff, volunteers and others vital to the continuity of care during the COVID-19 pandemic, especially those at New York Presbyterian/Columbia University Irving Medical Center.`,
 },]
 
-function ImgPlates({toggleIntro, windowWidth}){
+function ImgPlates({toggleIntro, windowWidth, showNext}){
 
 const transition_img = {              
     stiffness:314,
     damping: 26,
     mass: 2.3,
-    delay: 0,
+  
     type: "spring",
+    delay:0
 }
 
 let imageWidth = [ 
@@ -56,50 +58,76 @@ let imageWidth = [
     windowWidth*0.22,
     windowWidth*0.18 ]
 
+const getImgClass = (idx) => {
+    if (showNext) return `intro-img-${idx} intro-img-out`
+    if (toggleIntro) return `intro-img-${idx}`
+    return ""
+}
+
 
     return(
 
+        <AnimatePresence>
+
         <motion.div 
-        layout
-        className= {`images landing-images ${toggleIntro ? "intro-images" : ""}`}>
+        layout = "position"
+        initial= {{ opacity:1 }}
+        animate={{opacity:1}}
+        exit={{ opacity:0 }}
+        transition={{ delay:0, duration: 1, ease: "linear" }}
+
+        className= {`images landing-images ${toggleIntro ? "intro-images" : ""} ${showNext ? "intro-images-out" : ""}`}>
 
 
         <LayoutGroup>
 
         <motion.img 
-             layout
-             layoutId='img-0'
-             transition={transition_img}
-            className= {`landing-img-0 ${toggleIntro ? "intro-img-0" : ""}`}   src={plate0} width={ toggleIntro? imageWidth[0]*0.7 :imageWidth[0] } alt="plate" />
-        <motion.img 
-            layout
-            layoutId='img-1'
-            transition= {transition_img}
-            className= {`landing-img-1 ${toggleIntro ? "intro-img-1" : ""}`}   src={plate1} width={toggleIntro? imageWidth[1]*0.7 :imageWidth[1] } alt="plate" />
-        <motion.img 
-            layout
-            layoutId='img-2'
+             layout = "position"
+            layoutId='img-0'
+            animate={{opacity:1}}
+            exit={{ opacity:0 }}
             transition={transition_img}
-            className= {`landing-img-2 ${toggleIntro ? "intro-img-2" : ""}`}   src={plate2} width={toggleIntro? imageWidth[2]*0.7 :imageWidth[2] } alt="plate" />
+            className= {`landing-img-0 ${ getImgClass(0) }`} src={plate0} width={ toggleIntro? imageWidth[0]*0.7 :imageWidth[0] } alt="plate" />
         <motion.img 
-            layout
+             layout = "position"
+            layoutId='img-1'
+            animate={{opacity:1}}
+            exit={{ opacity:0 }}
+            transition= {transition_img}
+            className= {`landing-img-1 ${toggleIntro ? "intro-img-1" : ""} ${showNext ? "intro-img-out" : ""}`}   src={plate1} width={toggleIntro? imageWidth[1]*0.7 :imageWidth[1] } alt="plate" />
+        <motion.img 
+             layout = "position"
+            layoutId='img-2'
+            animate={{opacity:1}}
+            exit={{ opacity:0 }}
+            transition={transition_img}
+            className= {`landing-img-2 ${toggleIntro ? "intro-img-2" : ""} ${showNext ? "intro-img-out" : ""}`}   src={plate2} width={toggleIntro? imageWidth[2]*0.7 :imageWidth[2] } alt="plate" />
+        <motion.img 
+             layout = "position"
             layoutId='img-3'
+            animate={{opacity:1}}
+            exit={{ opacity:0 }}
             transition={transition_img}
         
-            className= {`landing-img-3 ${toggleIntro ? "intro-img-3" : ""}`}   src={plate3} width={toggleIntro? imageWidth[3]*0.7 :imageWidth[3] } alt="plate" />
+            className= {`landing-img-3 ${toggleIntro ? "intro-img-3" : ""} ${showNext ? "intro-img-out" : ""}`}   src={plate3} width={toggleIntro? imageWidth[3]*0.7 :imageWidth[3] } alt="plate" />
         <motion.img 
             layoutId='img-4'
-            layout
+            layout = "position"
+            animate={{opacity:1}}
+            exit={{ opacity:0 }}
             transition={transition_img}
-            className= {`landing-img-4 ${toggleIntro ? "intro-img-4" : ""}`}   src={plate4} width={toggleIntro? imageWidth[4]*0.7 :imageWidth[4] } alt="plate" />
+            className= {`landing-img-4 ${toggleIntro ? "intro-img-4" : ""} ${showNext ? "intro-img-out" : ""}`}   src={plate4} width={toggleIntro? imageWidth[4]*0.7 :imageWidth[4] } alt="plate" />
         <motion.img 
-            layout
+            layout = "position"
             layoutId='img-5'
+            animate={{opacity:1}}
+            exit={{ opacity:0 }}
             transition={transition_img}
-            className= {`landing-img-5 ${toggleIntro ? "intro-img-5" : ""}`}   src={plate5} width={toggleIntro? imageWidth[5]*0.7 :imageWidth[5] } alt="plate" />
+            className= {`landing-img-5 ${toggleIntro ? "intro-img-5" : ""} ${showNext ? "intro-img-out" : ""}`}   src={plate5} width={toggleIntro? imageWidth[5]*0.7 :imageWidth[5] } alt="plate" />
         
         </LayoutGroup>
         </motion.div>
+        </AnimatePresence>
     )
 }
 
@@ -113,20 +141,6 @@ function PlateTextEach({plateNum, windowHeight, windowWidth}){
     let left = plate["x"] * windowWidth + 'px';
     let top = plate["y"] * windowHeight + 'px' ;
 
-    const fadeInRight = {
-        hidden: {
-          opacity: 0,
-          x: -50
-        },
-        visible: {
-          opacity: 1,
-          x: 0,
-          transition: {
-            duration: 0.5
-          }
-        }
-      };
-
     return(      
     
     <motion.div  
@@ -134,7 +148,7 @@ function PlateTextEach({plateNum, windowHeight, windowWidth}){
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity:0,  x: 100,}}
-        transition={{ delay:0.5, duration: 1, ease:[0.58, -0.1, 0, 0.71] }}
+        transition={{ delay:0.5, duration: 2, ease:[0.58, -0.1, 0, 0.71] }}
 
         style={{ top: top, left:left}}
         className="intro-plate" id="intro-plate-wrapper">
@@ -163,21 +177,14 @@ function PlateTextEach({plateNum, windowHeight, windowWidth}){
 
 }
 
-function PlateText({prevScroll, windowHeight, windowWidth}){
+function PlateText({prevScroll, windowHeight, windowWidth, showNext, setNext}){
 
-    let navigate = useNavigate();
 
     const [plateNum, setPlateNum] = useState(undefined)
     const scrollPosition = useScrollPosition();
 
     useEffect(()=>{
-        
-        // document.getElementById("intro-plate-wrapper").style.transform = `translateY(-${(scrollPosition-prevScroll)}px)`;
-         
-        //navigation
-        // if (document.getElementById("intro-plate-wrapper").getBoundingClientRect().bottom <= windowHeight){
-        //     navigate("/visualization")
-        // }
+
         let process = (scrollPosition - prevScroll)/windowHeight; 
      
         if ( process < 0.1){
@@ -188,20 +195,25 @@ function PlateText({prevScroll, windowHeight, windowWidth}){
 
             if (plateNum !== 0)  setPlateNum(0)
            
-        }else if ( process < 0.6){
-            if (plateNum !== undefined)  setPlateNum(undefined)
+        // }else if ( process < 0.6){
+        //     if (plateNum !== undefined)  setPlateNum(undefined)
         }
         
         else if (process < 1){
             if (plateNum !== 1)  setPlateNum(1)
 
-        }else if ( process < 1.1){
-            if (plateNum !== undefined)  setPlateNum(undefined)
+        // }else if ( process < 1.1){
+        //     if (plateNum !== undefined)  setPlateNum(undefined)
         
         }else if (process < 1.5){
             if (plateNum !== 2)  setPlateNum(2)
+
+            if(showNext) setNext(false)
         }else{
-            navigate("/visualization")
+            if (!showNext) setNext(true);
+
+            if (plateNum !== undefined)  setPlateNum(undefined)
+        
         }
 
 
@@ -240,7 +252,7 @@ function Introduction(){
 
     const [prevScroll, setPrevScroll] = useState(0)
 
-
+    const [showNext, setNext] = useState(false)
 
     const handleResize = () => {
         setWindowHeight(window.innerHeight);
@@ -259,13 +271,16 @@ function Introduction(){
 
             bg.style.transform = ``;
 
+            setNext(false)
+            setIsPlate(false);
+
         }else if( scrollPosition > windowHeight && scrollPosition < windowHeight * 3){
 
             let prev_step = windowHeight
             bg.style.transform = `translateY(-${(scrollPosition-prev_step)*1.5}px)`;
 
         }
-        else if (scrollPosition >= window.innerHeight * 4.5){
+        else if (scrollPosition >= window.innerHeight * 5.8){
             navigate("/visualization")
         }else{
 
@@ -289,11 +304,23 @@ function Introduction(){
 
             if(isPlate){
                 setIsPlate(false)
-               
-            }
+                }
              
         }
 
+        //display next page
+
+
+
+        if (showNext){
+            let prev = prevScroll + 1.5 * windowHeight;
+            document.getElementById('intro-bg-rect-2').style.transform = `translateY(-${(scrollPosition-prev)*1.5}px)`;
+            document.getElementById('intro-content-2').style.transform = `translateY(-${(scrollPosition-prev)}px)`;
+            
+            if (document.getElementById('intro-bg-rect-2').getBoundingClientRect().bottom <= windowHeight* 0.6){
+                navigate("/visualization")
+            }
+        }
 
 
     }, [scrollPosition])
@@ -308,6 +335,7 @@ function Introduction(){
         >
             
             <div id ='intro-bg-rect'></div>
+            <div id ='intro-bg-rect-2'></div>
                     
             <div id='intro-content-0'>
 
@@ -334,12 +362,27 @@ function Introduction(){
                     </div>
             </div>
 
+            <div id='intro-content-2'>
+                    <div className='sectionTitle'>A Generated NYC Map</div>
+
+                    <div className='text-normal text-center'>
+
+                    Street names are how we recognize and navigate the physical spaces. They also manifest the intangibles, the local, historical, cultural, or political legacies of spaces.
+
+                    NYC honorary street names, or co-names by the City Council, are one way local activists, communities, and politicians lean on to remap our city in a local sense, interweaving its past, present, and future.
+
+                    </div>
+            </div>
+
             {isPlate &&
             <PlateText 
                 windowWidth= {windowWidth}
                 windowHeight= {windowHeight}
                 isPlate = {isPlate}
-                prevScroll= {prevScroll}/>    
+                prevScroll= {prevScroll}
+                showNext = {showNext}
+                setNext = {setNext}
+                />    
             }
        
         </motion.div>
@@ -347,6 +390,7 @@ function Introduction(){
         <ImgPlates 
             toggleIntro = {isPlate}
             windowWidth = {windowWidth}
+            showNext = {showNext}
         />      
 
         </React.Fragment>
